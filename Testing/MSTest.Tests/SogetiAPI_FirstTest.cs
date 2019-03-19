b89fd99f826b5e35ea524439cc8d67f5ff4df889
Alexander <Tyler.Alexander@us.sogeti.com>
2019-03-19 17:33:32 -0500

    Added MSTest to the project

---
+++ Testing/MSTest.Tests/SogetiAPI_FirstTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MSTest.Tests
+{
+    [TestClass]
+    public class SogetiAPI_FirstTest
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            bool does_this_work = true;
+            Assert.IsTrue(does_this_work);
+        }
+    }
+}
