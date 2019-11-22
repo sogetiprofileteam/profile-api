e750bef9ee6fdb28118493d06469fe4accbedc88
Liberty Failor <x-lfailor@cylancecorp.com>
2019-11-22 13:16:00 -0600

    v1

--- sogeti-portfolio-api/Models/Education.cs
+++ sogeti-portfolio-api/Models/Education.cs
@@ -5,7 +5,9 @@ namespace sogeti_portfolio_api.Models {
         public School school { get; set; }
         public string levelOfDegree { get; set; }
         public string subject { get; set; }
-        public DateTime startDate { get; set; }
-        public DateTime endDate { get; set; }
+        public int startMonth { get; set; }
+        public int startYear { get; set; }
+        public int endMonth { get; set; }
+        public int endYear { get; set; }
     }
 }
